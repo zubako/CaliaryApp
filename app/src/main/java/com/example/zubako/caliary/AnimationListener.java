0d3ba2e9c8d42875439d1140d2d6c37f029e52cf
zubako <zzz00900@gmail.com>
2018-05-22 18:05:28 +0900

    Initial commit

---
+++ app/src/main/java/com/example/zubako/caliary/AnimationListener.java
@@ -0,0 +1,21 @@
+package com.example.zubako.caliary;
+
+import android.view.animation.Animation;
+
+public abstract class AnimationListener implements Animation.AnimationListener{
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+}
