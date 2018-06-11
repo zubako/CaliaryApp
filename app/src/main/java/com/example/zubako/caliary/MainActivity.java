4ad8a48fd01d1164fb9ffa2935ea9b1771e3da04
410-33\user <dnfkzhwl@gmail.com>
2018-06-11 16:59:39 +0900

    리사이클뷰

--- app/src/main/java/com/example/zubako/caliary/MainActivity.java
+++ app/src/main/java/com/example/zubako/caliary/MainActivity.java
@@ -6,17 +6,27 @@ import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.zubako.caliary.R;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    SchItemList adapter;
+    ArrayList<Schedule> schedules = new ArrayList<>();
 
     final String LOG_TAG="myLogs";
     com.example.zubako.caliary.CompactCalendarView compactCalendar;
@@ -26,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.sch_recycler);
+        setData();
+        setRecyclerView();
 
         final ActionBar actionBar=getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(false);
@@ -41,11 +54,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDayClick(Date dateClicked){
                 Context context =getApplicationContext();
-                if(dateClicked.toString().compareTo("Fri Feb 10 18:00:00 AST 2017")==0){
+                if(dateClicked.toString().compareTo("Fri Feb 10 18:00:00 AST 2017")==0){// 요일 월 일 시:분:초: AST 년
                     Toast.makeText(context,"Teachers' Professional Day",Toast.LENGTH_SHORT).show();
                 }else{
                     Toast.makeText(context,"No Events Planned for that day",Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent( context, com.example.zubako.caliary.MainActivity2.class );
+                    Intent intent = new Intent( context, com.example.zubako.caliary.CSActivity.class );
                     startActivity( intent );
                     Log.d(LOG_TAG,""+dateClicked.toString().compareTo("Fri Feb 10 18:00:00 AST 2017"));
                 }
@@ -56,5 +69,25 @@ public class MainActivity extends AppCompatActivity {
                 actionBar.setTitle(dateFormatMonth.format(firstDayOfNewMonth));
             }
         });
+    }
+
+
+    void setRecyclerView(){
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new SchItemList(getApplicationContext(),schedules);
+        recyclerView.setAdapter(adapter);
+    }
+    void setData(){
+        Date currentTime = new Date ();
+        Schedule AA = new Schedule();
+        schedules.add(AA);
+        schedules.add(AA);
+        schedules.add(AA);
+        schedules.add(AA);
+        schedules.add(AA);
+        schedules.add(AA);
+
     }
 }
